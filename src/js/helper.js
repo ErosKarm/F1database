99c3d3dac49e5846dcfbb8f43265ad656c4ba813
ErosKarm <eroskarm@gmail.com>
2023-03-01 14:45:13 +0200

    Updates

--- src/js/helper.js
+++ src/js/helper.js
@@ -3,6 +3,8 @@ export const AJAX = async function (url) {
     const res = await fetch(url);
     const data = await res.json();
 
+    console.log(data);
+
     if (!res.ok) throw new Error(`${data.message}`);
   } catch (err) {
     throw err;
